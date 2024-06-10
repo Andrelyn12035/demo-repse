8e6eca097cdf4893470bccafd41a78b2009fbfe1
Andrelyn12035 <kevo1803wow@gmail.com>
2024-06-09 18:27:17 -0600

    avance filtros

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,33 +1,22 @@
+'use client';
 import InvoicesTable from '@/app/ui/dashboard/table';
 import { lusitana } from '@/app/ui/fonts';
-import { getDecIMSS } from '@/app/lib/actions';
-import { auth } from '@/auth';
-import { Button } from '../ui/button';
+import { useState } from 'react';
+export default function Page() {
+  const [rfc, setRfc] = useState('');
+  const [ejercicio, setEjercicio] = useState('');
+  const [periodo, setPeriodo] = useState('');
+  const handleRfc = () => {};
+  const handleEjercicio = () => {};
+  const handlePeriodo = () => {};
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams?: {
-    query?: string;
-    page?: string;
-  };
-}) {
-  const session = await auth();
-  if (session?.user) {
-  }
-
-  const query = searchParams?.query || '';
-  const currentPage = Number(searchParams?.page) || 1;
-  //const totalPages = await fetchInvoicesPages(query);
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-8 lg:grid-cols-8">
-        <InvoicesTable query={query} />
-        {/*currentPage={currentPage}*/}
-        {/*<LatestInvoices latestInvoices={latestInvoices} />*/}
+        <InvoicesTable rfc={rfc} ejercicio={ejercicio} periodo={periodo} />
       </div>
     </main>
   );
